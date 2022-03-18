efa23f98127ddc178a5982fad7b0a248a37d9c9a
RuslanHasiuk <54449805+RuslanHasiuk@users.noreply.github.com>
2022-03-18 15:10:15 +0200

    Update Loops While.py
    Generation range of numbers with specific "step" just by using built-in "range" function

--- Loops While.py
+++ Loops While.py
@@ -1,18 +1,24 @@
 # Вивести всі числа непарні числа від 100 до 140 (включно) , після цього ключового слова - [continue] - код ігнорується
-# number = 99
-# while number < 140:
-#     number += 1
-#     if (number % 2) == 0:
-#         continue
-#     print("LIne № " + str(number))
+# Print all odd numbers from 100 to 140 (inclusive), after this keyword - [continue] - the code is ignored
+number = 99
+while number < 140:
+    number += 1
+    if (number % 2) == 0:
+        continue
+    print("LIne № " + str(number))
 
-# вивести всі числа від 40 до 50
-# number = 40
-# while number >= 40:
-#     if number > 50:
-#         break
-#     print(number)
-#     number += 1
+
+# Or another way:
+range_from100_to140inc = range(101, 141, 2 )
+print(list(range_from100_to140inc))
+
+вивести всі числа від 40 до 50
+number = 40
+while number >= 40:
+    if number > 50:
+        break
+    print(number)
+    number += 1
 
 
 
