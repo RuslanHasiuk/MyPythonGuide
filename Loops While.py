7c7b3957fda4921934949b2c6141b1fc31aa38ae
RuslanHasiuk <54449805+RuslanHasiuk@users.noreply.github.com>
2022-03-18 15:11:13 +0200

    Update Loops While.py

--- Loops While.py
+++ Loops While.py
@@ -1,5 +1,5 @@
 # Вивести всі числа непарні числа від 100 до 140 (включно) , після цього ключового слова - [continue] - код ігнорується
-# Print all odd numbers from 100 to 140 (inclusive), after this keyword - [continue] - the code is ignored
+# Print all "odd" numbers from 100 to 140 (inclusive), after this keyword - [continue] - the code is ignored
 number = 99
 while number < 140:
     number += 1
@@ -11,6 +11,7 @@ while number < 140:
 # Or another way:
 range_from100_to140inc = range(101, 141, 2 )
 print(list(range_from100_to140inc))
+# Output: [101, 103, 105, 107, 109, 111, 113, 115, 117, 119, 121, 123, 125, 127, 129, 131, 133, 135, 137, 139]
 
 вивести всі числа від 40 до 50
 number = 40
