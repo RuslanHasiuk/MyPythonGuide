7b7eea4b8d374110d6f5d80e9b3529285f515d74
RuslanHasiuk <54449805+RuslanHasiuk@users.noreply.github.com>
2022-04-08 15:18:26 +0300

    Update Loops For  Practice #1.py
    Nested Loops refactoring

--- Loops For  Practice #1.py
+++ Loops For  Practice #1.py
@@ -73,6 +73,11 @@ print("---------------- Iteration of digitals in specific range (reversed order)
 for i in reversed(range(2, 9)):
     print(i)
 print('-------------- Nested Loops ----------------')
+
+sales_data = [[12, 17, 22], [2, 10, 3], [5, 12, 13]]
+
+scoops_sold = 0
+
 for location in sales_data:
     print(location)
     for element in location:
